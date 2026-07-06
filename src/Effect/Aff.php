98b09a492fd9afb0e7f58235c075ae031a672d5d
0x1 <kevin.francart3@gmail.com>
2026-07-06 18:58:45 +0200

    feat: migrate FFI to exports array format

--- src/Effect/Aff.php
+++ src/Effect/Aff.php
@@ -1,73 +1,92 @@
 <?php
 
-$Effect_Aff__pure = function($x) { return function() use(&$x) { return $x; }; };
-$Effect_Aff__map = function($f, $aff = null) {
+$_pure = function($x) use (&$_pure) { return function() use(&$x) { return $x; }; };
+$_map = function($f, $aff = null) use (&$_map) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__map;
-            return $Effect_Aff__map(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_pure) {
+
+            return $_map(...array_merge($__args, $more));
         };
     }
     return function() use(&$f, &$aff) { return $f($aff()); };
 };
-$Effect_Aff__bind = function($aff, $f = null) {
+$_bind = function($aff, $f = null) use (&$_bind) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__bind;
-            return $Effect_Aff__bind(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_map) {
+
+            return $_bind(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff, &$f) { return $f($aff())(); };
 };
-$Effect_Aff__liftEffect = function($eff) { return $eff; };
-$Effect_Aff__makeFiber = function($util, $aff) { return function() use(&$aff) { $fiber = new \Fiber($aff); $fiber->start(); return (object)['run' => function() {}, 'join' => function($k) { return function() { return function(){}; }; }]; }; };
-$Effect_Aff__fork = function($immediate, $aff = null) {
+$_liftEffect = function($eff) use (&$_liftEffect) { return $eff; };
+$_makeFiber = function($util, $aff) use (&$_makeFiber) { return function() use(&$aff) { $fiber = new \Fiber($aff); $fiber->start(); return (object)['run' => function() {}, 'join' => function($k) { return function() { return function(){}; }; }]; }; };
+$_fork = function($immediate, $aff = null) use (&$_fork) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__fork;
-            return $Effect_Aff__fork(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_bind) {
+
+            return $_fork(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff) { $fiber = new \Fiber($aff); \Revolt\EventLoop::queue(function() use(&$fiber) { $fiber->start(); }); return (object)['run' => function() {}, 'join' => function($k){ return function(){ return function(){}; }; }]; };
 };
-$Effect_Aff__delay = function($right, $ms) { return function() use(&$right, &$ms) { $fiber = \Fiber::getCurrent(); \Revolt\EventLoop::delay($ms / 1000, function() use(&$fiber, &$right) { if ($fiber) $fiber->resume(); }); if ($fiber) \Fiber::suspend(); return $right(null); }; };
-$Effect_Aff__makeSupervisedFiber = $Effect_Aff__makeFiber;
-$Effect_Aff__killAll = function($err, $sup, $cb) { return function() { return function(){}; }; };
-$Effect_Aff_makeAff = function($k) { return function() use(&$k) { return $k(function($res){ return function(){}; })(); }; };
-$Effect_Aff__throwError = function($err) { return function() use(&$err) { throw $err; }; };
-$Effect_Aff__catchError = function($aff, $f = null) {
+$_delay = function($right, $ms) use (&$_delay) { return function() use(&$right, &$ms) { $fiber = \Fiber::getCurrent(); \Revolt\EventLoop::delay($ms / 1000, function() use(&$fiber, &$right) { if ($fiber) $fiber->resume(); }); if ($fiber) \Fiber::suspend(); return $right(null); }; };
+$_makeSupervisedFiber = $_makeFiber;
+$_killAll = function($err, $sup, $cb) use (&$_killAll) { return function() { return function(){}; }; };
+$makeAff = function($k) use (&$makeAff) { return function() use(&$k) { return $k(function($res){ return function(){}; })(); }; };
+$_throwError = function($err) use (&$_throwError) { return function() use(&$err) { throw $err; }; };
+$_catchError = function($aff, $f = null) use (&$_catchError) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__catchError;
-            return $Effect_Aff__catchError(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_liftEffect) {
+
+            return $_catchError(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff, &$f) { try { return $aff(); } catch (\Throwable $e) { return $f($e)(); } };
 };
-$Effect_Aff_generalBracket = function($acq) { return function($cond) { return function($use) use(&$acq) { return function() use(&$acq, &$use) { $res = $acq(); return $use($res)(); }; }; }; };
-$Effect_Aff__parAffMap = $Effect_Aff__map;
-$Effect_Aff__parAffApply = function($aff1, $aff2 = null) {
+$generalBracket = function($acq) use (&$generalBracket) { return function($cond) { return function($use) use(&$acq) { return function() use(&$acq, &$use) { $res = $acq(); return $use($res)(); }; }; }; };
+$_parAffMap = $_map;
+$_parAffApply = function($aff1, $aff2 = null) use (&$_parAffApply) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__parAffApply;
-            return $Effect_Aff__parAffApply(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_makeFiber) {
+
+            return $_parAffApply(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff1, &$aff2) { return $aff1()($aff2()); };
 };
-$Effect_Aff__sequential = function($aff) { return $aff; };
-$Effect_Aff__parAffAlt = function($aff1, $aff2 = null) {
+$_sequential = function($aff) use (&$_sequential) { return $aff; };
+$_parAffAlt = function($aff1, $aff2 = null) use (&$_parAffAlt) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
-        return function(...$more) use ($__args) {
-            global $Effect_Aff__parAffAlt;
-            return $Effect_Aff__parAffAlt(...array_merge($__args, $more));
+        return function(...$more) use ($__args, &$_fork) {
+
+            return $_parAffAlt(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff1, &$aff2) { try { return $aff1(); } catch (\Throwable $e) { return $aff2(); } };
 };
+
+$exports['_pure'] = $_pure;
+$exports['_map'] = $_map;
+$exports['_bind'] = $_bind;
+$exports['_liftEffect'] = $_liftEffect;
+$exports['_makeFiber'] = $_makeFiber;
+$exports['_fork'] = $_fork;
+$exports['_delay'] = $_delay;
+$exports['_makeSupervisedFiber'] = $_makeSupervisedFiber;
+$exports['_killAll'] = $_killAll;
+$exports['makeAff'] = $makeAff;
+$exports['_throwError'] = $_throwError;
+$exports['_catchError'] = $_catchError;
+$exports['generalBracket'] = $generalBracket;
+$exports['_parAffMap'] = $_parAffMap;
+$exports['_parAffApply'] = $_parAffApply;
+$exports['_sequential'] = $_sequential;
+$exports['_parAffAlt'] = $_parAffAlt;
+return $exports;
