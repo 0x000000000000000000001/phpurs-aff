ec3465774dbb32e0c1f52b94d060551ffd11502c
0x1 <kevin.francart3@gmail.com>
2026-07-07 21:47:47 +0200

    feat: Implement concurrent Fibers and fix makeAff suspension

--- src/Effect/Aff.php
+++ src/Effect/Aff.php
@@ -5,7 +5,6 @@ $_map = function($f, $aff = null) use (&$_map) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_map) {
-
             return $_map(...array_merge($__args, $more));
         };
     }
@@ -15,34 +14,99 @@ $_bind = function($aff, $f = null) use (&$_bind) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_bind) {
-
             return $_bind(...array_merge($__args, $more));
         };
     }
     return function() use(&$aff, &$f) { return $f($aff())(); };
 };
 $_liftEffect = function($eff) use (&$_liftEffect) { return $eff; };
-$_makeFiber = function($util, $aff) use (&$_makeFiber) { return function() use(&$aff) { $fiber = new \Fiber($aff); $fiber->start(); return (object)['run' => function() {}, 'join' => function($k) { return function() { return function(){}; }; }]; }; };
+$_makeFiber = function($util, $aff) use (&$_makeFiber) { 
+    return function() use(&$aff) { 
+        $fiber = new \Fiber($aff); 
+        $fiber->start(); 
+        return (object)['run' => function() {}, 'join' => function($k) { return function() { return function(){}; }; }]; 
+    }; 
+};
 $_fork = function($immediate, $aff = null) use (&$_fork) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_fork) {
-
             return $_fork(...array_merge($__args, $more));
         };
     }
-    return function() use(&$aff) { $fiber = new \Fiber($aff); \Revolt\EventLoop::queue(function() use(&$fiber) { $fiber->start(); }); return (object)['run' => function() {}, 'join' => function($k){ return function(){ return function(){}; }; }]; };
+    return function() use(&$aff) { 
+        $fiber = new \Fiber($aff); 
+        \Revolt\EventLoop::queue(function() use(&$fiber) { $fiber->start(); }); 
+        return (object)['run' => function() {}, 'join' => function($k){ return function(){ return function(){}; }; }]; 
+    };
 };
-$_delay = function($right, $ms) use (&$_delay) { return function() use(&$right, &$ms) { $fiber = \Fiber::getCurrent(); \Revolt\EventLoop::delay($ms / 1000, function() use(&$fiber, &$right) { if ($fiber) $fiber->resume(); }); if ($fiber) \Fiber::suspend(); return $right(null); }; };
+$_delay = function($right, $ms) use (&$_delay) { 
+    return function() use(&$right, &$ms) { 
+        $fiber = \Fiber::getCurrent(); 
+        \Revolt\EventLoop::delay($ms / 1000, function() use(&$fiber, &$right) { 
+            if ($fiber) $fiber->resume(); 
+        }); 
+        if ($fiber) \Fiber::suspend(); 
+        return $right(null); 
+    }; 
+};
 $_makeSupervisedFiber = $_makeFiber;
 $_killAll = function($err, $sup, $cb) use (&$_killAll) { return function() { return function(){}; }; };
-$makeAff = function($k) use (&$makeAff) { return function() use(&$k) { return $k(function($res){ return function(){}; })(); }; };
+
+$makeAff = function($k) use (&$makeAff) { 
+    return function() use(&$k) { 
+        $fiber = \Fiber::getCurrent(); 
+        $isDone = false;
+        $result = null;
+        $exception = null;
+
+        $canceler = $k(function($res) use(&$fiber, &$isDone, &$result, &$exception) { 
+            return function() use(&$fiber, &$isDone, &$result, &$exception, &$res) { 
+                $isDone = true;
+                if (isset($res->tag) && $res->tag === 'Left') {
+                    $exception = $res->value0;
+                } else if (isset($res->tag) && $res->tag === 'Right') {
+                    $result = $res->value0;
+                } else if (isset($res->constructor) && $res->constructor === 'Left') {
+                    $exception = $res->value0;
+                } else if (isset($res->constructor) && $res->constructor === 'Right') {
+                    $result = $res->value0;
+                } else {
+                    $result = $res;
+                }
+                
+                if ($fiber && $fiber->isSuspended()) { 
+                    if ($exception !== null) {
+                        \Revolt\EventLoop::queue(function() use($fiber, $exception) {
+                            if ($fiber->isSuspended()) $fiber->throw($exception); 
+                        });
+                    } else {
+                        \Revolt\EventLoop::queue(function() use($fiber, $result) {
+                            if ($fiber->isSuspended()) $fiber->resume($result); 
+                        });
+                    }
+                } 
+            }; 
+        })(); 
+        
+        if (!$isDone) {
+            if ($fiber) {
+                return \Fiber::suspend(); 
+            } else {
+                throw new \RuntimeException("makeAff used outside of a fiber");
+            }
+        } else {
+            if ($exception !== null) throw $exception;
+            return $result;
+        }
+    }; 
+};
+
 $_throwError = function($err) use (&$_throwError) { return function() use(&$err) { throw $err; }; };
 $_catchError = function($aff, $f = null) use (&$_catchError) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_catchError) {
-
             return $_catchError(...array_merge($__args, $more));
         };
     }
@@ -50,26 +114,165 @@ $_catchError = function($aff, $f = null) use (&$_catchError) {
 };
 $generalBracket = function($acq) use (&$generalBracket) { return function($cond) { return function($use) use(&$acq) { return function() use(&$acq, &$use) { $res = $acq(); return $use($res)(); }; }; }; };
 $_parAffMap = $_map;
+
 $_parAffApply = function($aff1, $aff2 = null) use (&$_parAffApply) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_parAffApply) {
-
             return $_parAffApply(...array_merge($__args, $more));
         };
     }
-    return function() use(&$aff1, &$aff2) { return $aff1()($aff2()); };
+    return function() use(&$aff1, &$aff2) { 
+        $parent = \Fiber::getCurrent();
+        $isDone = false; 
+        $completed = 0;
+        $res1 = null;
+        $res2 = null;
+        $error = null;
+
+        $f1 = new \Fiber(function() use(&$aff1, &$isDone, &$completed, &$res1, &$error, $parent) {
+            try {
+                $res1 = $aff1();
+                if (!$isDone) {
+                    $completed++;
+                    if ($completed === 2) {
+                        $isDone = true;
+                        if ($parent && $parent->isSuspended()) {
+                            \Revolt\EventLoop::queue(function() use($parent) {
+                                if ($parent->isSuspended()) $parent->resume();
+                            });
+                        }
+                    }
+                }
+            } catch (\Throwable $e) {
+                if (!$isDone) {
+                    $isDone = true;
+                    $error = $e;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $e) {
+                            if ($parent->isSuspended()) $parent->throw($e);
+                        });
+                    }
+                }
+            }
+        });
+
+        $f2 = new \Fiber(function() use(&$aff2, &$isDone, &$completed, &$res2, &$error, $parent) {
+            try {
+                $res2 = $aff2();
+                if (!$isDone) {
+                    $completed++;
+                    if ($completed === 2) {
+                        $isDone = true;
+                        if ($parent && $parent->isSuspended()) {
+                            \Revolt\EventLoop::queue(function() use($parent) {
+                                if ($parent->isSuspended()) $parent->resume();
+                            });
+                        }
+                    }
+                }
+            } catch (\Throwable $e) {
+                if (!$isDone) {
+                    $isDone = true;
+                    $error = $e;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $e) {
+                            if ($parent->isSuspended()) $parent->throw($e);
+                        });
+                    }
+                }
+            }
+        });
+
+        \Revolt\EventLoop::queue(function() use($f1) { $f1->start(); });
+        \Revolt\EventLoop::queue(function() use($f2) { $f2->start(); });
+
+        if (!$isDone) {
+            \Fiber::suspend();
+        }
+        
+        if ($error !== null) throw $error;
+        return $res1($res2); 
+    };
 };
+
 $_sequential = function($aff) use (&$_sequential) { return $aff; };
+
 $_parAffAlt = function($aff1, $aff2 = null) use (&$_parAffAlt) {
     if (func_num_args() < 2) {
         $__args = func_get_args();
         return function(...$more) use ($__args, &$_parAffAlt) {
-
             return $_parAffAlt(...array_merge($__args, $more));
         };
     }
-    return function() use(&$aff1, &$aff2) { try { return $aff1(); } catch (\Throwable $e) { return $aff2(); } };
+    return function() use(&$aff1, &$aff2) { 
+        $parent = \Fiber::getCurrent();
+        $isDone = false;
+        $result = null;
+        $doneCount = 0;
+        $error2 = null;
+
+        $f1 = new \Fiber(function() use(&$aff1, &$isDone, &$result, &$doneCount, &$error2, $parent) {
+            try {
+                $res = $aff1();
+                if (!$isDone) {
+                    $isDone = true;
+                    $result = $res;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $result) {
+                            if ($parent->isSuspended()) $parent->resume($result);
+                        });
+                    }
+                }
+            } catch (\Throwable $e) {
+                $doneCount++;
+                if ($doneCount === 2 && !$isDone) {
+                    $isDone = true;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $error2) {
+                            if ($parent->isSuspended()) $parent->throw($error2); 
+                        });
+                    }
+                }
+            }
+        });
+
+        $f2 = new \Fiber(function() use(&$aff2, &$isDone, &$result, &$doneCount, &$error2, $parent) {
+            try {
+                $res = $aff2();
+                if (!$isDone) {
+                    $isDone = true;
+                    $result = $res;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $result) {
+                            if ($parent->isSuspended()) $parent->resume($result);
+                        });
+                    }
+                }
+            } catch (\Throwable $e) {
+                $error2 = $e;
+                $doneCount++;
+                if ($doneCount === 2 && !$isDone) {
+                    $isDone = true;
+                    if ($parent && $parent->isSuspended()) {
+                        \Revolt\EventLoop::queue(function() use($parent, $error2) {
+                            if ($parent->isSuspended()) $parent->throw($error2);
+                        });
+                    }
+                }
+            }
+        });
+
+        \Revolt\EventLoop::queue(function() use($f1) { $f1->start(); });
+        \Revolt\EventLoop::queue(function() use($f2) { $f2->start(); });
+
+        if (!$isDone) {
+            return \Fiber::suspend();
+        } else {
+            if ($doneCount === 2) throw $error2;
+            return $result;
+        }
+    };
 };
 
 $exports['_pure'] = $_pure;
